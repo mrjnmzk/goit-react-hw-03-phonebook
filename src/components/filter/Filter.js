39aba0eb484de5ed563a5d3b50b052cc99bc091c
marian muzyka <mrjnmzk@gmail.com>
2020-08-29 19:25:12 +0300

    qq

---
+++ src/components/filter/Filter.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Filter = ({ value, onChangeFilter }) => {
+  return (
+    <form>
+      <label>
+        Find contacts
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => onChangeFilter(e.target.value)}
+        ></input>
+      </label>
+    </form>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
+
+export default Filter;
